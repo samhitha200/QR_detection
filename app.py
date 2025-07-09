aee0ca36b110c68cb292de1ae25f78d78d94f611
samhitha200 <158454510+samhitha200@users.noreply.github.com>
2025-07-09 15:29:41 +0530

    app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ st.markdown("""
         text-align: center;
         font-size: 1rem;
         box-shadow: 0 0 10px rgba(0,0,0,0.15);
-        margin-top: 30px;
+        margin-top: 150px;
         border: 2px solid;
         width: 60%;
         margin-left: 160px;
